a660fab9ec29eb0d56370da968d12a337af9451d
Victor Guerra <vguerra@gmail.com>
2017-06-29 23:15:50 +0200

    using twiddle to tune the PID controller

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "json.hpp"
 #include "PID.h"
+#include "twiddle.h"
 #include <math.h>
 
 // for convenience
@@ -34,8 +35,13 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+//  pid.Init(0.2, 0.00005, 3.0);
+  pid.Init(0.0, 0.0, 0.0);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  Twiddle twiddle(pid, 500, 0.001);
+
+
+  h.onMessage([&twiddle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -57,15 +63,22 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          steer_value = -twiddle.DoStep(cte);
+
+          // DEBUG
+//          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+          std::string msg;
+          if (steer_value < -999.0) {
+            msg = "42[\"reset\",{}]";
+          } else {
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = 0.3;
+            msg = "42[\"steer\"," + msgJson.dump() + "]";
+          }
+//          std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
